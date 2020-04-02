2920e93168b01e6ee8333ce2bc3048c1cabf3555
Cristian Camilo Arenas Castellanos <cristianareca@unisabana.edu.co>
2020-04-02 18:58:21 -0500

    Modificado

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Pietrario'),
+          title: Text('Pietrariooo'),
           backgroundColor: Colors.black38,
         ),
       body: Container(
