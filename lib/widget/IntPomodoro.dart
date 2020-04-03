b320d83969a35a77e014e9e382e5a818de1fa6cd
Julian <julianvehe@unisabana.edu.co>
2020-04-02 21:03:25 -0500

    inter pomodoro

---
+++ lib/widget/IntPomodoro.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class IntPomodoro extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body:
+     OverflowBox(
+      child: Text('Hola mundo'),
+    )
+    );
+   
+  }
+}
--- lib/widget/onPietrario.dart
+++ lib/widget/onPietrario.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
+import 'IntPomodoro.dart';
 
 class PrincPietrario extends StatelessWidget {
+  void _startPomodoro(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            child: IntPomodoro(),
+            onTap: () {},
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
-          IconButton(
-            iconSize: 50,
-            icon: Icon(Icons.date_range),
+          FloatingActionButton(
+            child: Icon(Icons.calendar_today),
             onPressed: () {
-              // Pomodoro settings
+              _startPomodoro(context);
             },
           ),
           Row(
-   
             children: <Widget>[
               Container(
                 color: Colors.indigo,
-                 padding: EdgeInsets.only(right: 139),
+                padding: EdgeInsets.only(right: 139),
                 child: IconButton(
                   iconSize: 50,
-                  
                   icon: Icon(Icons.wb_sunny),
                   onPressed: () {
                     // Sun function displayed
@@ -34,7 +44,6 @@ class PrincPietrario extends StatelessWidget {
                 padding: EdgeInsets.only(left: 140),
                 child: IconButton(
                   iconSize: 50,
-                  
                   icon: Icon(Icons.build),
                   onPressed: () {
                     // Pietrario's config
