aab5bc2b3aec223077e146b9392ccced9ec44765
Julian <julianvehe@unisabana.edu.co>
2020-04-02 22:41:47 -0500

    add Date picker into Pomodoro Widget and extract the into from it

--- lib/widget/IntPomodoro.dart
+++ lib/widget/IntPomodoro.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
-class IntPomodoro extends StatelessWidget {
+import 'package:flutter_duration_picker/flutter_duration_picker.dart';
+
+class IntPomodoro extends StatefulWidget {
+
+  @override
+  _IntPomodoroState createState() => _IntPomodoroState();
+}
+
+class _IntPomodoroState extends State<IntPomodoro> {
+  Duration _duration = Duration(hours: 0, minutes: 0);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body:
-     OverflowBox(
-      child: Text('Hola mundo'),
-    )
-    );
-   
+    return Scaffold(
+        body: Container(
+          padding: EdgeInsets.only(left:90),
+          child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      
+      children: <Widget>[
+           DurationPicker(
+                duration: _duration,
+                onChange: (val) {
+                  this.setState(() => _duration = val);
+                },
+                snapToMins: 5.0,
+              ),
+              FlatButton(onPressed: () {
+                print(_duration);
+              },
+               child: Text('Push'))
+      ],
+    ),
+        ));
   }
 }
