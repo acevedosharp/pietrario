158855e6589e8803650d0005fe07d1b3b62b08c8
Julian <julianvehe@unisabana.edu.co>
2020-04-17 14:12:30 -0500

    transform to unity

---
+++ example/unity/practicaPomodoro/Assets/counterPicker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class counterPicker : MonoBehaviour
+{
+    float currentTime=0;
+    float startingTime=10;
+
+    [SerializeField] Text labelText;
+
+    void Start()
+    {
+      currentTime=startingTime;  
+    }
+    void Update()
+    {
+        currentTime -= 1*Time.deltaTime;
+        labelText.text=currentTime.ToString();
+    }
+    
+}
