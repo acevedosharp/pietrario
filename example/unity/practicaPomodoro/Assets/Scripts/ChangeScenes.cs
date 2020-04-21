32ec8d1398f4467707b89bc1cd9ad5fe2fc47a34
Cristian Camilo Arenas <cristianareca@unisabana.edu.co>
2020-04-21 17:45:21 -0500

    Creadas las escenas basicas conectadas entre si

--- example/unity/practicaPomodoro/Assets/Scripts/ChangeScenes.cs
+++ example/unity/practicaPomodoro/Assets/Scripts/ChangeScenes.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ChangeScenes : MonoBehaviour
 {
+
+     public void ChangeScenePietrarioList()
+    {
+        SceneManager.LoadScene(4);
+    }
     public void ChangeScenePomodoro()
     {
+        SceneManager.LoadScene(2);
+    }
+
+     public void ChangeSceneNewPietrario()
+    {
         SceneManager.LoadScene(1);
+    }
+
+    public void ChangeSceneStart()
+    {
+        SceneManager.LoadScene(0);
     }
 }
