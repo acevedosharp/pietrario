ebecf865e9c93f2040c96841ec20d013f3e9cacf
gf <f>
2020-04-20 19:01:26 -0500

    Implemented simple Persistence model for Pietrario. Can easily be improved upon.

---
+++ example/unity/practicaPomodoro/Assets/Scripts/persistence/entities/Pietrario.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace persistence.entities
+{
+    public class Pietrario
+    {
+        public int id;
+        public string Name;
+        public int S1, S2, S3;
+
+        public Pietrario(int id, string name, int s1, int s2, int s3)
+        {
+            this.id = id;
+            Name = name;
+            S1 = s1;
+            S2 = s2;
+            S3 = s3;
+        }
+
+        // This class needs a save method for persistence
+        public void Save()
+        {
+            PlayerPrefs.SetInt("id_pietrario_" + id, id);
+            PlayerPrefs.SetString("nombre_pietrario_" + id, Name);
+            PlayerPrefs.SetInt("id_suc_1_piet" + id, S1);
+            PlayerPrefs.SetInt("id_suc_2_piet" + id, S2);
+            PlayerPrefs.SetInt("id_suc_3_piet" + id, S3);
+        }
+    }
+}
---
+++ example/unity/practicaPomodoro/Assets/Scripts/persistence/entities/Succulent.cs
@@ -0,0 +1,21 @@
+﻿namespace persistence.entities
+{
+    public class Succulent
+    {
+        public int id;
+        public string Name;
+        public string ResourcePath;
+        public float WaterIndex;
+        public float SunLightIndex;
+
+        public Succulent(string name, string resourcePath, float waterIndex, float sunLightIndex)
+        {
+            Name = name;
+            ResourcePath = resourcePath;
+            WaterIndex = waterIndex;
+            SunLightIndex = sunLightIndex;
+        }
+        
+        // This class does NOT need a save method for persistence, it will be an immutable collection that lives only at runtime.
+    }
+}
