687f57a51855f5cbc051706f5d6d6ecdd78691c2
gf <f>
2020-04-22 19:58:25 -0500

    Basic lifecycle implementation & modifications of the basic persistence model to fit lifecycle requirements.
    Now Repositories are detached from scenes, currently inaccessible via Unity until new Pietrario and Pietrario view are connected.

--- example/unity/practicaPomodoro/Assets/Scripts/persistence/entities/Pietrario.cs
+++ example/unity/practicaPomodoro/Assets/Scripts/persistence/entities/Pietrario.cs
@@ -1,28 +1,86 @@
 ﻿using UnityEngine;
 
 
-    public class Pietrario
+public class Pietrario
+{
+    public int id;
+    public string name;
+    public long creationDate;
+    public long lastTimestamp;
+    public float humidityLevel;
+    public Succulent s1, s2, s3;
+    public float s1wl, s2wl, s3wl; // Water level
+    public float s1sl, s2sl, s3sl; // Sunlight level
+
+    public Pietrario(int id, string name, long creationDate, float humidityLevel, Succulent s1, Succulent s2, Succulent s3, float s1Wl, float s2Wl, float s3Wl, float s1Sl, float s2Sl, float s3Sl)
     {
-        public int id;
-        public string Name;
-        public int S1, S2, S3;
+        this.id = id;
+        this.name = name;
+        this.creationDate = creationDate;
+        this.humidityLevel = humidityLevel;
+        this.s1 = s1;
+        this.s2 = s2;
+        this.s3 = s3;
+        s1wl = s1Wl;
+        s2wl = s2Wl;
+        s3wl = s3Wl;
+        s1sl = s1Sl;
+        s2sl = s2Sl;
+        s3sl = s3Sl;
 
-        public Pietrario(int id, string name, int s1, int s2, int s3)
+        if (s1 != null)
         {
-            this.id = id;
-            Name = name;
-            S1 = s1;
-            S2 = s2;
-            S3 = s3;
+            s1.waterLevel = s1Wl;
+            s1.sunlightLevel = s1Sl;
         }
-
-        // This class needs a save method for persistence
-        public void Save()
+        if (s2 != null)
         {
-            PlayerPrefs.SetInt("id_pietrario_" + id, id);
-            PlayerPrefs.SetString("nombre_pietrario_" + id, Name);
-            PlayerPrefs.SetInt("id_suc_1_piet" + id, S1);
-            PlayerPrefs.SetInt("id_suc_2_piet" + id, S2);
-            PlayerPrefs.SetInt("id_suc_3_piet" + id, S3);
+            s2.waterLevel = s2Wl;
+            s2.sunlightLevel = s2Sl;
         }
+        if (s3 != null)
+        {
+            s3.waterLevel = s3Wl;
+            s3.sunlightLevel = s3Sl;
+        }
+        
+        lastTimestamp = creationDate;
     }
+
+    public void updateWithGivenTimeMilis(long timeToEvaluate)
+    {
+        long delta = timeToEvaluate - lastTimestamp;
+        
+        s1.update(delta);
+        s2.update(delta);
+        s3.update(delta);
+
+        lastTimestamp = timeToEvaluate;
+    }
+
+    // This class needs a save method for persistence
+    public void Save()
+    {
+        PlayerPrefs.SetInt("id_pietrario_" + id, id);
+        PlayerPrefs.SetString("nombre_pietrario_" + id, name);
+        PlayerPrefs.SetString("fecha_creacion_piet_" + id, creationDate.ToString()); // long value stored as string!!
+        PlayerPrefs.SetString("last_timestamp_piet_" + id, lastTimestamp.ToString()); // long value stored as string!!
+        PlayerPrefs.SetFloat("humidity_level_piet_" + id, humidityLevel);
+        PlayerPrefs.SetString("id_suc_1_piet_" + id, s1.persistentId);
+        PlayerPrefs.SetString("id_suc_2_piet_" + id, s2.persistentId);
+        PlayerPrefs.SetString("id_suc_3_piet_" + id, s3.persistentId);
+        
+        PlayerPrefs.SetFloat("s1wl_piet_" + id, s1wl);
+        PlayerPrefs.SetFloat("s2wl_piet_" + id, s2wl);
+        PlayerPrefs.SetFloat("s3wl_piet_" + id, s3wl);
+        
+        PlayerPrefs.SetFloat("s1sl_piet_" + id, s1sl);
+        PlayerPrefs.SetFloat("s2sl_piet_" + id, s2sl);
+        PlayerPrefs.SetFloat("s3sl_piet_" + id, s3sl);
+    }
+
+    public override string ToString()
+    {
+        return s1 +" | "+ s2 + " | "+ s3;
+    }
+}
--- example/unity/practicaPomodoro/Assets/Scripts/persistence/entities/Succulent.cs
+++ example/unity/practicaPomodoro/Assets/Scripts/persistence/entities/Succulent.cs
@@ -1,19 +1,35 @@
-﻿
-    public class Succulent
-    {
-        public int id;
-        public string Name;
-        public string ResourcePath;
-        public float WaterIndex;
-        public float SunLightIndex;
+﻿public class Succulent
+{
+    public string persistentId;
+    public string name;
+    public string resourcePath;
+    
+    public float waterLevel;
+    public float sunlightLevel;
+    
+    public float waterDecayIndex; //
+    public float sunlightDecayIndex;
 
-        public Succulent(string name, string resourcePath, float waterIndex, float sunLightIndex)
-        {
-            Name = name;
-            ResourcePath = resourcePath;
-            WaterIndex = waterIndex;
-            SunLightIndex = sunLightIndex;
-        }
-        
-        // This class does NOT need a save method for persistence, it will be an immutable collection that lives only at runtime.
+    public Succulent(string persistentId, string name, string resourcePath, float waterLevel, float sunlightLevel, float waterDecayIndex, float sunlightDecayIndex)
+    {
+        this.persistentId = persistentId;
+        this.name = name;
+        this.resourcePath = resourcePath;
+        this.waterLevel = waterLevel;
+        this.sunlightLevel = sunlightLevel;
+        this.waterDecayIndex = waterDecayIndex;
+        this.sunlightDecayIndex = sunlightDecayIndex;
     }
+
+    public void update(long timeDelta)
+    {
+        waterLevel -= waterDecayIndex * (timeDelta/(3600F*1000F));
+        sunlightLevel -= sunlightDecayIndex * (timeDelta/(3600F*1000F));
+    }
+    // This class does NOT need a save method for persistence, it will be part of an immutable collection that lives only at runtime.
+
+    public override string ToString()
+    {
+        return "Name: " + name + " - " + "Water level: " + waterLevel + " - " + "Sunlight level: " + sunlightLevel;
+    }
+}
