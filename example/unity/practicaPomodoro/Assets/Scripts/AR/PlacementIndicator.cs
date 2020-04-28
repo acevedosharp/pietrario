5a00e3fca4a590a06e699c5f8b44a8b01cecf0b9
Cristian Camilo Arenas <cristianareca@unisabana.edu.co>
2020-04-27 22:13:49 -0500

    Implementado Pietrario con AR v1

---
+++ example/unity/practicaPomodoro/Assets/Scripts/AR/PlacementIndicator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+public class PlacementIndicator : MonoBehaviour
+{
+
+    private ARRaycastManager raycastManager;
+    private GameObject placementIndicator;
+    
+    void Start()
+    {
+        //encuentro el objeto de ARRaycastManager
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+       //encuentro el objeto hijo de mi gameobject
+        placementIndicator = transform.GetChild(0).gameObject;
+        //coloco por defecto el indicador invisible
+        placementIndicator.SetActive(false);
+    }
+
+    private void Update()
+    {
+        
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        //creo mi raycast con una posicion, el detector de físico y el tipo de superficies en las que se va a activar
+        raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2),hits,TrackableType.Planes);
+
+        if (hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if (!placementIndicator.activeInHierarchy)
+            {
+                placementIndicator.SetActive(true);
+            }
+        }
+
+    }
+}
---
+++ example/unity/practicaPomodoro/Assets/Scripts/AR/SpawnPietrario.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPietrario : MonoBehaviour
+{
+
+    public GameObject pietrario;
+
+    private PlacementIndicator placementIndicator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject gameObject = Instantiate(pietrario, placementIndicator.transform.position,
+                placementIndicator.transform.rotation);
+        }
+    }
+}
