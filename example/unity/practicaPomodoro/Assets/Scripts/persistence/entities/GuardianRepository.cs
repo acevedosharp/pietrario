36e8fa2f0fa4f7ae294f1689b71e3e107f8b380d
acevedosharp <acevedosharp@hotmail.com>
2020-05-01 12:09:12 -0500

    Fixed previous commit

---
+++ example/unity/practicaPomodoro/Assets/Scripts/persistence/entities/GuardianRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GuardianRepository
+{
+    // Acá se añaden las suculentas.
+    public static Guardian[] guardianes =
+    {
+        new Guardian("GUA1", "Trigrito", "Grrrrr") 
+    };
+
+    public static Guardian find(string id)
+    {
+        // Es importante retornar una copia de la Suculenta (no una referencia), puesto que sobre esta se van a realizar algunas operaciones y no queremos modificar los objetos de este repositorio.
+        Guardian referencedGuardian = guardianes.First(g => g.id.Equals(id));
+        return new Guardian(
+            referencedGuardian.id,
+            referencedGuardian.name,
+            referencedGuardian.mensaje
+        );
+    }
+}
---
+++ example/unity/practicaPomodoro/Assets/Scripts/persistence/entities/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Inventory
+{
+    // El id inicia con GUA para guardianes y con SUC para suculentas
+    public static int getCantidadByReferencedItem(string refId)
+    {
+        return PlayerPrefs.GetInt("inv_quantity_" + refId);
+    }
+
+    public static void addCantidadByReferencedItem(string refId, int cantidad)
+    {
+        PlayerPrefs.SetInt("inv_quantity_" + refId, cantidad);
+    }
+}
