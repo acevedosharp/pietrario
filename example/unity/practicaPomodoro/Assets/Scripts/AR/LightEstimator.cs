741612821f32fcd512530b216f617380234b3c6b
acevedosharp <acevedosharp@hotmail.com>
2020-05-01 20:22:11 -0500

    Add files via upload

---
+++ example/unity/practicaPomodoro/Assets/Scripts/AR/LightEstimator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class LightEstimator : MonoBehaviour
+{
+    [SerializeField] private ARCameraManager _arCameraManager;
+
+    [SerializeField] private Text brightnessPreview;
+
+    private Light _light;
+
+    private void Awake()
+    {
+        _light = GetComponent<Light>();
+    }
+
+    private void OnEnable()
+    {
+        _arCameraManager.frameReceived += FrameUpdated;
+    }
+
+    private void OnDisable()
+    {
+        _arCameraManager.frameReceived -= FrameUpdated;
+    }
+
+    private void FrameUpdated(ARCameraFrameEventArgs args)
+    {
+        if (args.lightEstimation.averageBrightness.HasValue)
+        {
+            float lightValue = args.lightEstimation.averageBrightness.Value;
+            _light.intensity = lightValue;
+
+
+            brightnessPreview.text = lightValue.ToString();
+        }
+    }
+}
