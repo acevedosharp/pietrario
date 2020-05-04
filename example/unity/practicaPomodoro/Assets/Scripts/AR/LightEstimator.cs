741638c724f3fff9b79cad7dcfab35503f88ce25
acevedosharp <acevedosharp@hotmail.com>
2020-05-04 09:30:43 -0500

    Now you can let your Pietrario get some sun light!

--- example/unity/practicaPomodoro/Assets/Scripts/AR/LightEstimator.cs
+++ example/unity/practicaPomodoro/Assets/Scripts/AR/LightEstimator.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
@@ -8,13 +7,16 @@ public class LightEstimator : MonoBehaviour
 {
     [SerializeField] private ARCameraManager _arCameraManager;
 
-    [SerializeField] private Text brightnessPreview;
+    [SerializeField] private Text accumulatorPreview;
 
-    private Light _light;
+    [SerializeField] private Image sunIconIndicator;
 
-    private void Awake()
+    private int frameCounter = 1;
+
+    private void Start()
     {
-        _light = GetComponent<Light>();
+        PlayerPrefs.SetString("last_accumulator_timestamp", "0");
+        PlayerPrefs.SetInt("current_light_accumulator_value", 0);
     }
 
     private void OnEnable()
@@ -29,13 +31,38 @@ public class LightEstimator : MonoBehaviour
 
     private void FrameUpdated(ARCameraFrameEventArgs args)
     {
-        if (args.lightEstimation.averageBrightness.HasValue)
+        // Cada 60 frames
+        if (frameCounter % 60 == 0 && args.lightEstimation.averageBrightness.HasValue)
         {
             float lightValue = args.lightEstimation.averageBrightness.Value;
-            _light.intensity = lightValue;
+            accumulatorPreview.text = PlayerPrefs.GetInt("current_light_accumulator_value") + " Lux";
 
-
-            brightnessPreview.text = lightValue.ToString();
+            attemptUpdateAccumulator(lightValue);
         }
+
+        frameCounter++;
+    }
+
+    private void attemptUpdateAccumulator(float lightValue)
+    {
+        // Si ha pasado por lo menos 1s desde la última actualización, se actualizará.
+        if (DateTime.Now.Ticks - long.Parse(PlayerPrefs.GetString("last_accumulator_timestamp")) > 1000)
+        {
+            if (lightValue > 0.3)
+            {
+                PlayerPrefs.SetString("last_accumulator_timestamp", DateTime.Now.Ticks.ToString());
+                int currentAccValue = PlayerPrefs.GetInt("current_light_accumulator_value");
+                if (currentAccValue < 100)
+                    PlayerPrefs.SetInt("current_light_accumulator_value", currentAccValue + 1);
+                
+                sunIconIndicator.color = Color.white;
+            }
+            else
+            {
+                sunIconIndicator.color = Color.grey;
+            }
+            
+        }
+        
     }
 }
