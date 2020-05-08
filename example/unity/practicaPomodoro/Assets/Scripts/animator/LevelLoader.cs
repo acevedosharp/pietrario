02059aa6a36ef739bf38753af774afe2e11b605e
acevedosharp <acevedosharp@hotmail.com>
2020-05-08 10:58:18 -0500

    fixed?

---
+++ example/unity/practicaPomodoro/Assets/Scripts/animator/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    
+    public void ChangeSceneWithTransition(int sceneIndex)
+    {
+        StartCoroutine(LoadNewSceneCoroutine(sceneIndex));
+    }
+
+    IEnumerator LoadNewSceneCoroutine(int sceneIndex)
+    {
+        transition.SetTrigger("OnChangeSceneRequested");
+        
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
