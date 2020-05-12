d74de3bfd9f171e74926ec7ab0cf552f863ea4d0
Cristian Camilo Arenas <cristianareca@unisabana.edu.co>
2020-05-12 14:23:11 -0500

    Implementado inventario de agua, correcciones visuales

--- example/unity/practicaPomodoro/Assets/Scripts/UpdateToolButtons.cs
+++ example/unity/practicaPomodoro/Assets/Scripts/UpdateToolButtons.cs
@@ -5,36 +5,43 @@ using UnityEngine;
 public class UpdateToolButtons : MonoBehaviour
 {
   public GameObject tool1;
-  
   public GameObject tool2;
-  
   public GameObject tool3;
-
+  public GameObject notool3;
+  public GameObject notool2;
   public GameObject notool1;
 
-  public GameObject notool2;
-
-  public GameObject notool3;
-  
   private bool water_available;
 
 
-  public void Check()
+  public void Start()
   {
-    water_available = Inventory.getCantidadByReferencedItem("AGUA") > 0;
-    UpdateButtons();
+    tool1.SetActive(false);
+    notool1.SetActive(false);
   }
 
-  void UpdateButtons()
+  public void ShowButton()
   {
+    water_available = Inventory.getCantidadByReferencedItem("AGUA") > 0;
     if (water_available)
     {
       tool1.SetActive(true);
-      tool2.SetActive(true);
-      tool3.SetActive(true);
+    }
+    else
+    {
+      notool1.SetActive(true);
+    }
+  }
+
+  public void UpdateButton()
+  {
+    water_available = Inventory.getCantidadByReferencedItem("AGUA") > 0;
+    
+    if (water_available)
+    {
+      tool1.SetActive(true);
       notool1.SetActive(false);
-      notool2.SetActive(false);
-      notool3.SetActive(false);
+     
     }
     else
     {
---
+++ example/unity/practicaPomodoro/Assets/Scripts/UpdateToolButtons3.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateToolButtons3 : MonoBehaviour
+{
+    public GameObject tool2;
+    public GameObject tool1;
+    public GameObject tool3;
+    public GameObject notool1;
+    public GameObject notool2;
+    public GameObject notool3;
+
+    private bool water_available;
+
+
+    public void Start()
+    {
+        tool3.SetActive(false);
+        notool3.SetActive(false);
+    }
+
+    public void ShowButton()
+    {
+        water_available = Inventory.getCantidadByReferencedItem("AGUA") > 0;
+        if (water_available)
+        {
+            tool3.SetActive(true);
+        }
+        else
+        {
+            notool3.SetActive(true);
+        }
+    }
+
+    public void UpdateButton()
+    {
+        water_available = Inventory.getCantidadByReferencedItem("AGUA") > 0;
+    
+        if (water_available)
+        {
+            tool3.SetActive(true);
+            notool3.SetActive(false);
+     
+        }
+        else
+        {
+            notool1.SetActive(true);
+            notool2.SetActive(true);
+            notool3.SetActive(true);
+            tool3.SetActive(false);
+            tool2.SetActive(false);
+            tool1.SetActive(false);
+        }
+    
+    }
+}
